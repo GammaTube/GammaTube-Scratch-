d0d44dd297b269c8ac8e6976cdcfa7d5c42a1638
uukelele-scratch <robustrobot11@gmail.com>
2025-01-03 14:46:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ def video_data(id):
 	return ["frame-1-data", "frame-2-data", "frame-3-data", "etc."] # frame data for each frame
 
 @client.request
-def audio_data(id)
+def audio_data(id):
 	print("audio_data for " + id)
 	return ["idk", "what", "format", "this", "will", "be"] # idk what format this will be
 
 @client.request
-def video_metadata(id)
+def video_metadata(id):
 	print("metadata for " + id)
 	return ["video id for reference", "video title", "video duration", "video thumbnail data", "views", "likes", "channel name", "channel thumbnail data", "creation date"]
 
