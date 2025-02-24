4b96f34d18971c2897126e494011a22b75488d5b
uukelele-scratch <robustrobot11@gmail.com>
2025-02-24 08:34:07 +0000

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,6 +1,7 @@
 import requests
 from PIL import Image
 from io import BytesIO
+from moviepy.editor import VideoFileClip
 
 def _rgb2scratch(rgb):
     r, g, b = rgb
@@ -15,10 +16,22 @@ def _process_frame(image, size):
       rgb = resized_img.getpixel((x, y))[:3]
       row.append(_rgb2scratch(rgb))
     image_data.append(row)
-  return image_data
+  return str(image_data)
 
 def img_from_url(url, size):
   response = requests.get(url)
   img = Image.open(BytesIO(response.content))
   img = img.convert('RGB')
   return _process_frame(img, size)
+
+def vid(filepath, fps=1, size=32):
+    clip = VideoFileClip(filepath)
+    clip = clip.resize((size, size)).set_fps(fps)
+    frames_data = []
+    	for frame in clip.iter_frames(fps=fps, dtype="uint8"):
+		    pil_image = Image.fromarray(frame)
+		    frame_data = _process_frame(pil_image, size)
+		    frames_data.append(frame_data)
+    
+    clip.close()
+    return frames_data
